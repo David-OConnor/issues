888672d1dd2d879d13e7791a5e07d17cecc76391
David O'Connor <david.alan.oconnor@gmail.com>
2020-03-14 16:27:31 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Issues
+## Similar to Github Issues, but for local businesses
---
+++ main/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+
+class CategoryChoice(models.IntegerChoices):
+    RESTAURANT = 0
+    COFFEE_SHOP = 2
+    CLOTHING = 3
+
+
+class Business(models.Model):
+    name = models.CharField(max_length=30)
+    category = models.IntegerChoices(choicdes=CategoryChoice.choices)
+
+    # Address info
+    # todo: table for cities etc?
+    country = models.IntegerField()  # ICAO country code. (Which format?)
+    city = models.CharField(max_length=30)
+    street = models.CharField(max_length=50)
+    address_num = models.CharField(max_length=30)
+    postal_code = models.CharField(max_length=30)
+
+
+
+class Issues(models.Model):
+    
